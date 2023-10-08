ca42a7ec233ca59166881bb8bc5a984f9e68fc3a
DanCRk <dancrkyt@gmail.com>
2023-10-07 22:07:11 -0600

    YA SIRVE PAGAR PRESTAMOS

--- src/app/modules/cuenta-usuario/secciones/pagar-prestamo/pagar-prestamo.component.ts
+++ src/app/modules/cuenta-usuario/secciones/pagar-prestamo/pagar-prestamo.component.ts
@@ -17,15 +17,7 @@ export class PagarPrestamoComponent {
 
   checkInputs(): boolean {
     let pasa = true;
-    if (this.monto < 0) {
-      pasa = false;
-      this.util.enviarAlerta(
-        'warning',
-        this.util.colorWarning,
-        'Error en el formulario',
-        'El monto no puede ser negativo'
-      );
-    } else if (this.concepto.length < 0) {
+ if (this.concepto.length < 0) {
       pasa = false;
       this.util.enviarAlerta(
         'warning',
@@ -59,12 +51,11 @@ export class PagarPrestamoComponent {
         descripcion: this.descripcion,
         fecha_transaccion: null,
         id_transaccion: null,
-        monto: this.monto,
+        monto: this.usuario.usuarioss[0]?.prestamos[0]?.pago_mensual,
         nombre_destinatario: null,
         nombre_remitente: null,
         tipo: 3,
       };
-      console.log(transaccion);
       this.usuario.sendTransaccion(transaccion).subscribe({
         next: (e) => {
           this.util.enviarAlerta('success', '#7fffc1', 'Exito!', e.mensaje);
@@ -84,7 +75,25 @@ export class PagarPrestamoComponent {
           });
         },
         error: (e) => {
-          this.util.enviarAlerta('warning', '#ff8100', '', e.error.mensaje);
+          if(e.codigo==1012){
+            this.util.enviarAlerta('success', '#7fffc1', 'Exito!', e.mensaje);
+            this.usuario.getUsuario(sessionStorage.getItem('idUser')).subscribe({
+              next: (e) => {
+                this.usuario.usuarioss.push(e);
+                let cadena = e.cuenta.numeroCuenta;
+                let resultado = cadena.match(/.{1,4}/g);
+                this.usuario.usuarioss[0].cuenta.numeroCuenta =
+                  resultado.join(' ');
+                cadena = e.tarjetaCredito?.numero_tarjeta;
+                this.usuario.usuarioss[0].tarjetaCredito.numero_tarjeta = cadena
+                  .match(/.{1,4}/g)
+                  .join(' ');
+              },
+              error: (e) => {},
+            });
+          }else{
+            this.util.enviarAlerta('warning', '#ff8100', '', e.error.mensaje);
+          }
         },
       });
     }
